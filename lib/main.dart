283b7d667d25eeffd6fdc17cd80d10266beecc03
Lorem404 <waiyanaung@myansis.com>
2024-01-09 10:10:31 +0630

    project setup

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import "package:firebase_core/firebase_core.dart";
+import "package:flutter/material.dart";
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      color: Colors.black,
+    );
+  }
+}
